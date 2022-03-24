b182597dc2c551992fefb2eb51c1ca21891ffc62
bichanna <nobu.bichanna@gmail.com>
2022-03-24 18:15:18 -0500

    Add roman2int proc

--- src/romanim.nim
+++ src/romanim.nim
@@ -1,7 +1,28 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+#[
+  romanim.nim
+  Created by Nobuharu Shimazu on 3/24/2022
+]#
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+import tables
+
+const roman: Table[string, int] = 
+  {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}.toTable
+
+proc roman2int*(str: string): int =
+  var i = 0;
+
+  while i < len(str):
+    if i + 1 < len(str) and roman.hasKey(str[i..i+1]):
+      result += roman[str[i..i+1]]
+      i += 2
+    elif i < len(str) and roman.hasKey($str[i]):
+      result += roman[$str[i]]
+      i += 1
+    else:
+      break
+
+proc int2roman*(num: int): int =
+  discard
+
+proc isRoman*(str: string): bool =
+  discard
