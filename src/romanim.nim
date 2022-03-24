7a88abee17abe8a93d962b99ffe0b466b5521e09
bichanna <nobu.bichanna@gmail.com>
2022-03-24 18:59:29 -0500

    Add int2roman

--- src/romanim.nim
+++ src/romanim.nim
@@ -5,12 +5,25 @@
 
 import tables
 
-const roman*: Table[string, int] = 
-  {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}.toTable
+const roman*: OrderedTable[string, int] = [
+  ("M",  1000),
+  ("CM", 900),
+  ("D",  500),
+  ("CD", 400),
+  ("C",  100),
+  ("XC", 90),
+  ("L",  50),
+  ("XL", 40),
+  ("X",  10),
+  ("IX", 9),
+  ("V",  5),
+  ("IV", 4),
+  ("I",  1),
+].toOrderedTable
 
 var isStrict* = false
 
-type NotRomanNumException* = object of CatchableError
+type RomanNumException* = object of CatchableError
 
 proc roman2int*(str: string): int =
   var i = 0;
@@ -24,13 +37,21 @@ proc roman2int*(str: string): int =
       i += 1
     else:
       if isStrict:
-        raise newException(NotRomanNumException, "Not a Roman numeral")
+        raise newException(RomanNumException, "Not Roman numerals: '" & str & "'")
       else:
-        # There's no concept of 0 and negative numbers in Roman numerals
         return -1
 
-proc int2roman*(num: int): int =
-  discard
+proc int2roman*(num: int): string =
+  if num <= 0:
+    raise newException(RomanNumException, "Cannot convert 0 or negative numbers to Roman numerals")
+  
+  var copyNum = num;
+
+  while copyNum > 0:
+    for r, n in roman:
+      while copyNum >= n:
+        result &= r
+        copyNum -= n
 
 proc isRoman*(str: string): bool =
   discard
