3c0650679eff4308fc5d2b5dd2c2253d0c7901c7
bichanna <nobu.bichanna@gmail.com>
2022-03-24 18:24:24 -0500

    Add isStrict and NotRomanNumException

--- src/romanim.nim
+++ src/romanim.nim
@@ -5,8 +5,12 @@
 
 import tables
 
-const roman: Table[string, int] = 
+const roman*: Table[string, int] = 
   {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}.toTable
+
+var isStrict* = false
+
+type NotRomanNumException* = object of CatchableError
 
 proc roman2int*(str: string): int =
   var i = 0;
@@ -19,7 +23,11 @@ proc roman2int*(str: string): int =
       result += roman[$str[i]]
       i += 1
     else:
-      break
+      if isStrict:
+        raise newException(NotRomanNumException, "Not a Roman numeral")
+      else:
+        # There's no concept of 0 and negative numbers in Roman numerals
+        return -1
 
 proc int2roman*(num: int): int =
   discard
