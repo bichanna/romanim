a543718e4817857fff21a1db16112e5b94147d59
bichanna <nobu.bichanna@gmail.com>
2022-03-24 19:02:19 -0500

    Add comments

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,9 +1,7 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
+#[
+  test1.nim
+  Created by Nobuharu Shimazu on 3/24/2022
+]#
 
 import unittest
 
